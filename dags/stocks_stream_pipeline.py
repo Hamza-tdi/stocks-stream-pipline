804d22bb46b32cdd2523514d4bf2ebfc779b33fd
hamza <hamza.el.bahlaoui@outlook.com>
2024-12-02 22:58:18 +0100

    db tables creation and added more data to fetch func

--- dags/stocks_stream_pipeline.py
+++ dags/stocks_stream_pipeline.py
@@ -5,57 +5,189 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.postgres_operator import PostgresOperator
-# from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import Variable
+from psycopg2.errors import NotNullViolation
 import requests
 def store_data_task(**kwargs):
-    records = kwargs.get['ti'].xcom_pull(key='records')
+    records = kwargs.get('ti').xcom_pull(key='records')
+    peers = kwargs.get('ti').xcom_pull(key='peers')
+    recommended_trends = kwargs.get('ti').xcom_pull(key='recommendation_trends')
     pg_hook = PostgresHook(postgres_conn_id='postgres_local')
     for record in records:
-        pg_hook.run("""
-                        INSERT INTO stocks_data (
-                            symbol, current_price, change, percent_change,
-                            high_price_of_the_day, low_price_of_the_day,
-                            open_price_of_the_day, previous_close_price
+        try:
+            pg_hook.run("""
+                INSERT INTO stocks_data (
+                    id,
+                    symbol,
+                    name,
+                    current_price,
+                    change,
+                    percent_change,
+                    high_price_of_the_day,
+                    low_price_of_the_day,
+                    open_price_of_the_day,
+                    previous_close_price,
+                    timestamp
+                ) VALUES (
+                    %(id)s,
+                    %(symbol)s,
+                    %(name)s,
+                    %(current_price)s,
+                    %(change)s,
+                    %(percent_change)s,
+                    %(high_price_of_the_day)s,
+                    %(low_price_of_the_day)s,
+                    %(open_price_of_the_day)s,
+                    %(previous_close_price)s,
+                    %(timestamp)s
+                )
+                ON CONFLICT (id) DO UPDATE SET
+                    symbol = EXCLUDED.symbol,
+                    name = EXCLUDED.name,
+                    current_price = EXCLUDED.current_price,
+                    change = EXCLUDED.change,
+                    percent_change = EXCLUDED.percent_change,
+                    high_price_of_the_day = EXCLUDED.high_price_of_the_day,
+                    low_price_of_the_day = EXCLUDED.low_price_of_the_day,
+                    open_price_of_the_day = EXCLUDED.open_price_of_the_day,
+                    previous_close_price = EXCLUDED.previous_close_price,
+                    timestamp = EXCLUDED.timestamp;
+            """, parameters=record)
+            for item in recommended_trends:
+                for record in item:
+                    pg_hook.run("""
+                        INSERT INTO recommendation_trends (
+                            symbol,
+                            buy,
+                            hold,
+                            sell,
+                            strong_buy,
+                            strong_sell,
+                            period
                         ) VALUES (
-                            %(symbol)s, %(current_price)s, %(change)s, %(percent_change)s,
-                            %(high_price_of_the_day)s, %(low_price_of_the_day)s,
-                            %(open_price_of_the_day)s, %(previous_close_price)s
+                            %(symbol)s,
+                            %(buy)s,
+                            %(hold)s,
+                            %(sell)s,
+                            %(strong_buy)s,
+                            %(strong_sell)s,
+                            %(period)s
                         )
-                        ON CONFLICT (symbol) DO UPDATE SET
-                            current_price = EXCLUDED.current_price,
-                            change = EXCLUDED.change,
-                            percent_change = EXCLUDED.percent_change,
-                            high_price_of_the_day = EXCLUDED.high_price_of_the_day,
-                            low_price_of_the_day = EXCLUDED.low_price_of_the_day,
-                            open_price_of_the_day = EXCLUDED.open_price_of_the_day,
-                            previous_close_price = EXCLUDED.previous_close_price
-                    """, record)
-        print("Data inserted successfully.")
+                        ON CONFLICT (symbol, period) DO UPDATE SET
+                            buy = EXCLUDED.buy,
+                            hold = EXCLUDED.hold,
+                            sell = EXCLUDED.sell,
+                            strong_buy = EXCLUDED.strong_buy,
+                            strong_sell = EXCLUDED.strong_sell;
+                    """, parameters=record)
+            for peer in peers:
+                pg_hook.run("""
+                    INSERT INTO peers (
+                        symbol,
+                        sector,
+                        industry,
+                        subIndustry
+                    ) VALUES (
+                        %(symbol)s,
+                        %(sector)s,
+                        %(industry)s,
+                        %(subIndustry)s
+                    )
+                    ON CONFLICT (symbol) DO UPDATE SET
+                        sector = EXCLUDED.sector,
+                        industry = EXCLUDED.industry,
+                        subIndustry = EXCLUDED.subIndustry;
+                """, parameters=peer)
+            print("Data inserted successfully.")
+        except NotNullViolation as e:
+            print('Record contains Null levels, Skipped.')
 def fetch_data_task(**kwargs):
     print('Fetching Data')
     variables = Variable.get('variables', deserialize_json=True)
     url = variables[0]['url']
     token = variables[0]['api_key']
+    # Fetch Recommendations
     records = []
-    for i in range(len(variables[0]['companies'][0:3])):
+    for i in range(len(variables[0]['companies'][0:10])):
+        recommendation_trends = variables[0]['recommendation_trends'].replace('{SYMBOL}', variables[0]['companies'][i]['symbol'])
+        res = requests.get(f'{url}{recommendation_trends}{token}')
+        res_raw = res.json()
+        if res.status_code == 429:
+            print(f'API Limit Reached')
+            time.sleep(10)
+        elif res.status_code == 200:
+            record = []
+            for elm in res_raw:
+                item = {
+                    'symbol': variables[0]['companies'][i]['symbol'],
+                    'buy': elm['buy'],
+                    'sell': elm['sell'],
+                    'hold': elm['hold'],
+                    'strong_buy': elm['strongBuy'],
+                    'strong_sell': elm['strongSell'],
+                    'period': elm['period']
+                }
+                record.append(item)
+            print(record)
+            records.append(record)
+        else:
+            print(f"Unexpected status code: {res.status_code}")
+            return None
+    kwargs['ti'].xcom_push(key='recommendation_trends', value=records)
+
+    # Fetch Peers
+    records = []
+    for i in range(len(variables[0]['companies'][0:10])):
+        groups = []
+        for item in ['sector', 'industry', 'subIndustry']:
+            peers = variables[0]['peers'].replace('{SYMBOL}', variables[0]['companies'][i]['symbol'])
+            peers = peers.replace('{GROUPING}', item)
+            res = requests.get(f'{url}{peers}{token}')
+            res_raw = res.json()
+            if res.status_code == 429:
+                print(f'API Limit Reached')
+                time.sleep(10)
+            elif res.status_code == 200:
+                groups.append(res_raw)
+            else:
+                print(f"Unexpected status code: {res.status_code}")
+                return None
+        try:
+            record = {
+                'symbol': variables[0]['companies'][i]['symbol'],
+                'sector': groups[0],
+                'industry': groups[1],
+                'subIndustry': groups[2]
+            }
+            print(record)
+            records.append(record)
+        except IndexError as e:
+            print('Empty List, Symbol has no data')
+    kwargs['ti'].xcom_push(key='peers', value=records)
+
+    # Fetch Quotes
+    records = []
+    for i in range(len(variables[0]['companies'][0:10])):
         quote = variables[0]['quote'].replace('{SYMBOL}', variables[0]['companies'][i]['symbol'])
         res = requests.get(f'{url}{quote}{token}')
+        res_raw = res.json()
         if res.status_code == 429:
             print(f'API Limit Reached')
             time.sleep(10)
         elif res.status_code == 200:
             record = {
-                'id': f"{variables[0]['companies'][i]['symbol']}_{res['t']}",
+                'id': f"{variables[0]['companies'][i]['symbol']}_{res_raw['t']}",
                 'symbol': variables[0]['companies'][i]['symbol'],
-                'current_price': res['c'],
-                'change': res['d'],
-                'percent_change': res['d'],
-                'high_price_of_the_day': res['h'],
-                'low_price_of_the_day': res['l'],
-                'open_price_of_the_day': res['o'],
-                'previous_close_price': res['pc'],
-                'timestamp': res['t']
+                'name': variables[0]['companies'][i]['name'],
+                'current_price': res_raw['c'],
+                'change': res_raw['d'],
+                'percent_change': res_raw['dp'],
+                'high_price_of_the_day': res_raw['h'],
+                'low_price_of_the_day': res_raw['l'],
+                'open_price_of_the_day': res_raw['o'],
+                'previous_close_price': res_raw['pc'],
+                'timestamp': res_raw['t']
             }
             print(record)
             records.append(record)
@@ -90,33 +222,59 @@ default_args={
 },
     catchup=False
                         ) as f:
-    fetch_data =PythonOperator(
-        task_id="fetch_data",
-        python_callable=fetch_data,
-        provide_context=True,
+    fetch_data_task =PythonOperator(
+task_id="fetch_data_task",
+python_callable=fetch_data_task,
+provide_context=True,
     )
     check_market_status_task = PythonOperator(
         task_id="check_market_status_task",
         python_callable=check_market_status_task,
         provide_context=True
     )
-    create_table_task = PostgresOperator(
-        task_id='create_postgres_stocks_table',
-        postgres_conn_id='postgres_local',
-        sql='''
-        CREATE TABLE IF NOT EXISTS stocks_data (
-            id VARCHAR(25) PRIMARY KEY,
-            symbol VARCHAR(10),
-            current_price DECIMAL(10, 2) NOT NULL,
-            change DECIMAL(10, 2) NOT NULL,
-            percent_change DECIMAL(5, 2) NOT NULL,
-            high_price_of_the_day DECIMAL(10, 2),
-            low_price_of_the_day DECIMAL(10, 2),
-            open_price_of_the_day DECIMAL(10, 2),
-            previous_close_price DECIMAL(10, 2),
-            timestamp BIGINT
-        )
-        '''
+    store_data_task = PythonOperator(
+        task_id="store_data_task",
+        python_callable=store_data_task,
+        provide_context=True
     )
+    create_tables_task = PostgresOperator(
+        task_id="create_postgres_tables",
+        postgres_conn_id="postgres_local",
+        sql=
+        '''
+            CREATE TABLE IF NOT EXISTS stocks_data (
+                id VARCHAR(25) PRIMARY KEY,
+                symbol VARCHAR(10),
+                name VARCHAR(255),
+                current_price DECIMAL(10, 2) NOT NULL,
+                change DECIMAL(10, 2) NOT NULL,
+                percent_change DECIMAL(5, 2) NOT NULL,
+                high_price_of_the_day DECIMAL(10, 2),
+                low_price_of_the_day DECIMAL(10, 2),
+                open_price_of_the_day DECIMAL(10, 2),
+                previous_close_price DECIMAL(10, 2),
+                timestamp BIGINT
+            );
+            
+            CREATE TABLE IF NOT EXISTS recommendation_trends (
+                symbol VARCHAR(10),
+                buy INT,
+                hold INT,
+                sell INT,
+                strong_buy INT,
+                strong_sell INT,
+                period DATE,
+                primary key (symbol, period)
+            );
+            
+            CREATE TABLE IF NOT EXISTS peers(
+                symbol VARCHAR(10) PRIMARY KEY,
+                sector TEXT[][],
+                industry TEXT[][],
+                subIndustry TEXT[][]
+            )
+        '''
+)
 
-create_table_task >> check_market_status_task >> fetch_data_task >>store_data_task
+
+create_tables_task >> check_market_status_task >> fetch_data_task >> store_data_task
